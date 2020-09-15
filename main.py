46a791eb4902b401a844124cb49cf1aacf4826ce
SUNTheBaer <padron1999@yahoo.com>
2020-09-15 13:37:17 -0400

    Displays website name when password is returned

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ if not website_exists:
     password_length = input("How long would you like the password to be?\n")
     new_password = generate_password(password_length)
     fo.write(website_name + " " + new_password + "\n")
-    print("Your new password is: " + new_password)
+    print("Your new password for " + website_name + " is: " + new_password)
 else:
     fo.seek(0, 0)
     for i in fo.readlines():
         if website_name in i:
-            print("Your password is: " + i.split()[1])
+            print("Your password for " + i.split()[0] + " is: " + i.split()[1])
 
 fo.close();
